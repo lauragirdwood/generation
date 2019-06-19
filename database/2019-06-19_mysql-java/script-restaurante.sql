f72fe706a2708c27fecd8515152c9df2d1fd5ae0
Laura Girdwood <azevedoemello@gmail.com>
2019-06-19 11:01:26 -0300

    add script exemplo restaurante

---
+++ database/2019-06-19_mysql-java/script-restaurante.sql
@@ -0,0 +1,23 @@
+-- CREATE DATABASE restaurante;
+-- SHOW DATABASES;
+-- USE restaurante;
+
+/*
+CREATE TABLE cliente (
+	id_cliente INTEGER NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id_cliente)
+);
+*/
+
+-- DESCRIBE cliente;
+
+/*
+INSERT INTO cliente (id_cliente, nome)
+VALUES (1, "João");
+
+INSERT INTO cliente (id_cliente, nome)
+VALUES (2, "Maria");
+
+SELECT * FROM cliente;
+*/
